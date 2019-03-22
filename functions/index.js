53ebc55ef3fd1d55a3c94c6633ba6b88bb9172f5
Irit <35077819+iritush@users.noreply.github.com>
2019-03-21 23:55:17 -0400

    Writing config values to console to try using environment configuration data in functions. This worked on my local environment but needs to be deployed and tested with the server.Added runtimeconfig.json to .gitignore to make sure it isn't accidently checked in as this file stores the configuration data when testing functions locally.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,17 @@
 const functions = require('firebase-functions');
 
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
  exports.helloWorld = functions.https.onRequest((request, response) => {
 	 response.send("Hello from Firebase!");
+
+		console.log("Attempting to access environment configuration data") //Must be configured on the firebase side using the firebase cli: https://firebase.google.com/docs/functions/config-env
+		 try {
+			 console.log(`user: ${functions.config().credentials.user} password: ${functions.config().credentials.password}`)
+		 }
+		 catch (error) {
+			 console.log(`Got the following error when trying to access credentials: ${{error}}`)
+		 }
  });
+ 
