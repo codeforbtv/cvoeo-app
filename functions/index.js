fce54120241b94f92f587a30dc3b7a5da345ff95
Irit <35077819+iritush@users.noreply.github.com>
2019-04-20 23:25:20 -0400

    Add a function that connects to the sftp server and lists the root of the file system. Using an http request to trigger thre function for the time being

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,38 @@
 const functions = require('firebase-functions');
+const client = require('ssh2-sftp-client');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-	 response.send("Hello from Firebase!");
-
-		console.log("Attempting to access environment configuration data") //Must be configured on the firebase side using the firebase cli: https://firebase.google.com/docs/functions/config-env
-		 try {
-			 console.log(`user: ${functions.config().credentials.user} password: ${functions.config().credentials.password}`)
-		 }
-		 catch (error) {
-			 console.log(`Got the following error when trying to access credentials: ${{error}}`)
-		 }
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send("Hello from Firebase!");
+	console.log("Attempting to access environment configuration data") //Must be configured on the firebase side using the firebase cli: https://firebase.google.com/docs/functions/config-env
+	 try {
+		 console.log(`user: ${functions.config().credentials.user} password: ${functions.config().credentials.password}`)
+	 }
+	 catch (error) {
+		 console.log(`Got the following error when trying to access credentials: ${{error}}`)
+	 }
+});
+ 
+exports.sftpConnect = functions.https.onRequest((request, response) => {
+	 var sftp = new client();
+	 //connect to cvoeo sftp server
+	 sftp.connect({
+		 host: `${functions.config().serverinfo.host}`,
+		 username: `${functions.config().serverinfo.username}`,
+		 password: `${functions.config().serverinfo.password}`
+	 })
+	 .then(() => {
+		 return sftp.list('/');
+	 })
+	 .then((data) => {
+		 console.log(data, 'the data info');
+	        response.send(data);
+	 })
+	 .then(() => {
+		 return sftp.end();
+	 })
+	 .catch((error) => {
+		 console.log(`Got the following error when trying to connect to the sftp server: ${{error}}`)
+		 return sftp.end();
+	 });
  });
  
