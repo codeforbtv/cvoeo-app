0fadc4030e4002a3f74a45eb5c258802be397ffb
Micah Mutrux <micah.mutrux@gmail.com>
2019-11-03 11:39:58 -0500

    add simple exampleFunction for others to start from

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
+const exampleFunction = require('./examples/exampleFunction');
 const ingestCSV = require('./ingestion/ingestCSV');
 
+/*
+ * To add a new function take a look at how `exampleFunction` is created and exported.
+ */
+
 module.exports = {
+  'exampleFunction': exampleFunction,
   'ingestCSV': ingestCSV
 };
