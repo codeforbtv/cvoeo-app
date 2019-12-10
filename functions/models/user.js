a621a429afaaddd4fb8558ffadb080e9033d1f63
Irit <35077819+iritush@users.noreply.github.com>
2019-12-10 10:19:23 -0500

    add 2 new methods to the user class: createNewUserInFirestore and updateExistingUserInFirestore

--- functions/models/user.js
+++ functions/models/user.js
@@ -1,17 +1,41 @@
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const db = admin.firestore();
+let usersCollection = db.collection('users');
 class User {
     constructor() {
         this.uid = '';
         this.email = '';
         this.firstName = '';
         this.lastName = '';
-        this.dateCreated = Date.now();
+        this.dateCreated = Date.now();//TODO: switch this to a more readable date format
     }
-    get uid() {
-        return this._uid;
-    }
-    set uid(value) {
-        this._uid = value;
-      } 
-}
 
+    createNewUserInFirestore() {
+        usersCollection.doc(this.uid).set({
+            created: this.dateCreated,
+            uid: this.uid,
+            displayName: this.firstName,
+            lastName: this.lastName,
+            email: this.email
+          });
+    }
+
+    updateExistingUserInFirestore () {
+      if (this.firstName) {
+        console.log ("Updating first name");          
+        usersCollection.doc(this.uid).update({displayName: this.firstName});
+      }
+      if (this.lastName) {
+        console.log ("Updating Last name");          
+        usersCollection.doc(this.uid).update({lastName: this.lastName});
+      }
+      if (this.email) {
+        console.log ("Updating email address");          
+        usersCollection.doc(this.uid).update({email: this.email});
+      }
+    }
+}
 module.exports = User;
+module.exports.db = db;
