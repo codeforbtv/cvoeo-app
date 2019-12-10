3de4c67b6adb6fbdb37735628e415758cd842c4f
Irit <35077819+iritush@users.noreply.github.com>
2019-12-10 10:18:10 -0500

    rename file from models/validators.js -> models/data-validators.js and add a TODO item

--- functions/models/data-validators.js
+++ functions/models/data-validators.js
@@ -1,3 +1,4 @@
+// TODO look into using an existing library instead:  https://blog.mailtrap.io/react-native-email-validation
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const isValidEmail = (address) => emailRegex.test(address);
